bbc79763f9163bb237e3ba43769f0a38bee22e23
Nicholas Dear <nicholasdear@aol.com>
2025-02-19 16:54:59 -0500

    added all the files and finished server.ts

---
+++ server/src/service/weatherService.ts
@@ -0,0 +1,80 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+// TODO: Define an interface for the Coordinates object
+
+interface Coordinates {
+  lat: number;
+  lon: number;
+}
+
+// TODO: Define a class for the Weather object
+
+class Weather {
+  temperature: number;
+  description: string;
+  humidity: number;
+  windSpeed: number;
+
+// TODO: Complete the WeatherService class
+
+constructor(temperature: number, description: string, humidity: number, windSpeed: number) {
+  this.temperature = temperature;
+  this.description = description;
+  this.humidity = humidity;
+  this.windSpeed = windSpeed;
+}
+}
+
+class WeatherService {
+  // TODO: Define the baseURL, API key, and city name properties
+  
+  private baseURL: string;
+  private apiKey: string;
+  private cityName: string;
+
+  constructor() {
+    this.baseURL = 'https://api.openweathermap.org/data/2.5';
+    this.apiKey = process.env.WEATHER_API_KEY || '';
+    this.cityName = '';
+  }
+
+  // TODO: Create fetchLocationData method
+
+  private async fetchLocationData(query: string) {}
+
+  // TODO: Create destructureLocationData method
+
+  private destructureLocationData(locationData: Coordinates): Coordinates {}
+
+  // TODO: Create buildGeocodeQuery method
+
+  private buildGeocodeQuery(): string {}
+
+  // TODO: Create buildWeatherQuery method
+
+  private buildWeatherQuery(coordinates: Coordinates): string {}
+
+  // TODO: Create fetchAndDestructureLocationData method
+
+  private async fetchAndDestructureLocationData() {}
+
+  // TODO: Create fetchWeatherData method
+
+  private async fetchWeatherData(coordinates: Coordinates) {}
+
+  // TODO: Build parseCurrentWeather method
+
+  private parseCurrentWeather(response: any) {}
+
+  // TODO: Complete buildForecastArray method
+
+  private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
+
+  // TODO: Complete getWeatherForCity method
+
+  async getWeatherForCity(city: string) {}
+
+}
+
+export default new WeatherService();
