8bedd7ffaba2d126c203a0d12639cac206639a24
Nicholas Dear <nicholasdear@aol.com>
2025-02-25 19:19:02 -0500

    fixed error with file path

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
 
-app.use(express.static('client/dist'));
+app.use(express.static('../client/dist'));
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
 
