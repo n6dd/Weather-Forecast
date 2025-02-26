e7af7c5956c4765ff7736318e31cc4b9fc11f3d5
Nicholas Dear <nicholasdear@aol.com>
2025-02-25 19:23:56 -0500

    fixed weatherRoutes

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -3,14 +3,13 @@ const router = Router();
 
 import HistoryService from '../../service/historyService.js';
 import WeatherService from '../../service/weatherService.js';
-import historyService from '../../service/historyService.js';
 
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', async (req, res) => {
   try {
     const data = await WeatherService.getWeatherForCity(req.body.cityName)
     res.status(200).json(data || [])
-    await historyService.addCity(req.body.cityName)
+    await HistoryService.addCity(req.body.cityName)
   } catch (err) {
     console.error(err)
     res.status(500).json(err)
@@ -35,7 +34,7 @@ router.get('/history', async (_req, res) => {
 router.delete('/history/:id', async (req, res) => {
   try {
     console.log('History route', req.params.id)
-    await historyService.removeCity(req.params.id)
+    await HistoryService.removeCity(req.params.id)
     res.status(200).json([])
   } catch (err) {
     console.error(err)
